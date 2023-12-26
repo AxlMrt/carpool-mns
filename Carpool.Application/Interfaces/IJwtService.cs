3de9ac40636514bfa51ab7dc8290a49a06f02422
Axel Martin <martin.axel@sfr.fr>
2023-12-26 17:31:43 +0100

    Add IJwtService

---
+++ Carpool.Application/Interfaces/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Carpool.Application;
+
+public interface IJwtService
+{
+    Task<string> GenerateToken(string userId, string userEmail);
+    Task<ClaimsPrincipal> GetPrincipalFromTokenAsync(string token);
+
+}
