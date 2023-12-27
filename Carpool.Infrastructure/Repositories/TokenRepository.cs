594b787a20eaa80c9df611852e74e8cb51518f9c
Axel Martin <martin.axel@sfr.fr>
2023-12-27 11:08:14 +0100

    Add Token repository

---
+++ Carpool.Infrastructure/Repositories/TokenRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Carpool.Infrastructure;
+
+public class TokenRepository : ITokenRepository
+{
+    private readonly Dictionary<string, string> _tokens = new Dictionary<string, string>();
+
+    public async Task SaveTokenAsync(string userId, string token)
+    {
+        _tokens[userId] = token;
+        await Task.CompletedTask;
+    }
+
+    public async Task<string> GetTokenByUserIdAsync(string userId)
+    {
+        return await Task.FromResult(_tokens.ContainsKey(userId) ? _tokens[userId] : null);
+    }
+
+    public async Task RemoveTokenAsync(string userId)
+    {
+        if (_tokens.ContainsKey(userId))
+            _tokens.Remove(userId);
+        
+        await Task.CompletedTask;
+    }
+}
