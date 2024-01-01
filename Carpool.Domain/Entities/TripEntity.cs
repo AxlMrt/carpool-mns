b75f1c7249838cd2ad026d8c178e2b25c3fb0a5d
Axel Martin <martin.axel@sfr.fr>
2024-01-01 10:18:22 +0100

    Update DbContext

--- Carpool.Domain/Entities/TripEntity.cs
+++ Carpool.Domain/Entities/TripEntity.cs
@@ -6,8 +6,15 @@ namespace Carpool.Domain.Entities
         public Guid DriverId { get; set; }
         public User Driver { get; set; }
 
+        public Guid? CarId { get; set; }
+        public Car Car { get; set; }
+
+        public Guid? DepartureAddressId { get; set; }
         public Address DepartureAddress { get; set; }
+
+        public Guid? DestinationAddressId { get; set; }
         public Address DestinationAddress { get; set; }
+
         public DateTime DepartureTime { get; set; }
         public int AvailableSeats { get; set; }
         public bool IsSmokingAllowed { get; set; }
