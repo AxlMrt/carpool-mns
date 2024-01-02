5c980cee1d3c06e5567b880af440b6635fc53497
Axel Martin <martin.axel@sfr.fr>
2024-01-02 11:48:26 +0100

    Update car service and controller

--- Carpool.API/Controllers/CarController.cs
+++ Carpool.API/Controllers/CarController.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Carpool.Application.Interfaces;
 using Carpool.Domain.Entities;
+using Carpool.Application.Exceptions;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Authorization;
+using Carpool.Domain.Roles;
 
 namespace Carpool.API.Controllers
 {
-    [ApiController]
-    [Route("api/cars")]
+    [Authorize]
     public class CarController : BaseApiController
     {
         private readonly ICarService _carService;
@@ -15,6 +18,26 @@ namespace Carpool.API.Controllers
             _carService = carService;
         }
 
+        [Authorize(Roles = Roles.Administrator)]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetAllCars()
+        {
+            try
+            {
+                IEnumerable<Car> cars = await _carService.GetAllCarsAsync();
+                return Ok(cars);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while fetching all the cars: {ex.Message}");
+            }
+        }
+
+        [Authorize(Roles = Roles.Administrator)]
         [HttpGet("user/{userId}")]
         public async Task<ActionResult<IEnumerable<Car>>> GetCarsByUserId(Guid userId)
         {
@@ -23,26 +46,41 @@ namespace Carpool.API.Controllers
                 var cars = await _carService.GetCarsByUserIdAsync(userId);
                 return Ok(cars);
             }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
+                return StatusCode(500, $"An error occurred while fetching user car: {ex.Message}");
             }
         }
 
+        [Authorize(Roles = Roles.Administrator)]
         [HttpGet("{id}")]
         public async Task<ActionResult<Car>> GetCarById(Guid id)
         {
             try
             {
                 var car = await _carService.GetCarByIdAsync(id);
-                if (car == null)
-                    return NotFound();
 
                 return Ok(car);
             }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
+                return StatusCode(500, $"An error occurred while fetching car: {ex.Message}");
             }
         }
 
@@ -56,7 +94,7 @@ namespace Carpool.API.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
+                return StatusCode(500, $"An error occurred while creating the car: {ex.Message}");
             }
         }
 
@@ -65,15 +103,20 @@ namespace Carpool.API.Controllers
         {
             try
             {
-                if (id != car.Id)
-                    return BadRequest();
-
                 await _carService.UpdateCarAsync(id, car);
                 return NoContent();
             }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
+                return StatusCode(500, $"An error occurred while updating the car: {ex.Message}");
             }
         }
 
@@ -85,9 +128,17 @@ namespace Carpool.API.Controllers
                 await _carService.DeleteCarAsync(id);
                 return NoContent();
             }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred: {ex.Message}");
+                return StatusCode(500, $"An error occurred while removing the car: {ex.Message}");
             }
         }
     }
--- Carpool.Application/Services/CarServices.cs
+++ Carpool.Application/Services/CarServices.cs
@@ -2,42 +2,64 @@ using Carpool.Application.Exceptions;
 using Carpool.Application.Interfaces;
 using Carpool.Domain.Entities;
 using Carpool.Domain.Interfaces;
+using Carpool.Domain.Roles;
+using Carpool.Infrastructure.Interfaces;
+using Carpool.Infrastructure.Repositories;
 
 namespace Carpool.Application.Services
 {
     public class CarService : ICarService
     {
         private readonly ICarRepository _carRepository;
+        private readonly IUserRepository _userRepository;
 
-        public CarService(ICarRepository carRepository)
+        public CarService(ICarRepository carRepository, IUserRepository userRepository)
         {
             _carRepository = carRepository;
+            _userRepository = userRepository;
         }
 
         public async Task<IEnumerable<Car>> GetAllCarsAsync()
         {
-            return await _carRepository.GetAllCarsAsync();
+            IEnumerable<Car> cars = await _carRepository.GetAllCarsAsync();
+            if (cars is null || !cars.Any())
+                throw new NotFoundException("No cars found in database.");
+
+            return cars;
         }
 
         public async Task<Car> GetCarByIdAsync(Guid id)
         {
+            if (id == Guid.Empty)
+                throw new BadRequestException("Empty ID is not allowed.");
+
             return await _carRepository.GetCarByIdAsync(id);
         }
 
         public async Task<IEnumerable<Car>> GetCarsByUserIdAsync(Guid userId)
         {
+            if (userId == Guid.Empty)
+                throw new BadRequestException("Empty ID is not allowed.");
+
+            User user = await _userRepository.GetUserByIdAsync(userId) ?? throw new NotFoundException($"User with ID {userId} not found.");
             return await _carRepository.GetCarsByUserIdAsync(userId);
         }
 
         public async Task<Car> CreateCarAsync(Car car)
         {
+            if (car is null)
+                throw new BadRequestException("Car object cannot be null.");
+
             await _carRepository.CreateCarAsync(car);
             return car;
         }
 
         public async Task<Car> UpdateCarAsync(Guid id, Car car)
         {
-            var existingCar = await _carRepository.GetCarByIdAsync(id) ?? throw new NotFoundException($"Car with ID {id} not found.");
+            if (id == Guid.Empty)
+                throw new BadRequestException("Empty ID is not allowed.");
+
+            Car existingCar = await _carRepository.GetCarByIdAsync(car.Id) ?? throw new NotFoundException($"Car with ID {id} not found.");
 
             car.Id = existingCar.Id;
             await _carRepository.UpdateCarAsync(car);
@@ -46,6 +68,9 @@ namespace Carpool.Application.Services
 
         public async Task<bool> DeleteCarAsync(Guid id)
         {
+            if (id == Guid.Empty)
+                throw new BadRequestException("Empty ID is not allowed.");
+
             var existingCar = await _carRepository.GetCarByIdAsync(id) ?? throw new NotFoundException($"Car with ID {id} not found.");
 
             await _carRepository.DeleteCarAsync(id);
