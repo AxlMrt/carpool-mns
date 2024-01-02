3fc7d2f40cc3b83589e084d3071a536cafdce339
Axel Martin <martin.axel@sfr.fr>
2024-01-02 16:41:10 +0100

    Create Address dto

---
+++ Carpool.Domain/DTOs/AddressDTOs/AddressDto.cs
@@ -0,0 +1,12 @@
+namespace Carpool.Domain.DTOs
+{
+    public class AddressDto
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
