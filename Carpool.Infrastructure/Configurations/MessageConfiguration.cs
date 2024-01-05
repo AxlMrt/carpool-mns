78556ae7090002aed5620735c1b83b5b5c8ef67d
Axel Martin <martin.axel@sfr.fr>
2024-01-05 15:56:40 +0100

    Add Message entity to db context and create configuration

---
+++ Carpool.Infrastructure/Configurations/MessageConfiguration.cs
@@ -0,0 +1,38 @@
+using Carpool.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Carpool.Infrastructure.Configurations
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Id)
+                .IsRequired();
+
+            builder.Property(m => m.Content)
+                .IsRequired();
+
+            builder.Property(m => m.TimeStamp)
+                .IsRequired();
+
+            builder.HasOne(m => m.Sender)
+                .WithMany()
+                .HasForeignKey(m => m.SenderId)
+                .IsRequired();
+
+            builder.HasOne(m => m.Recipient)
+                .WithMany()
+                .HasForeignKey(m => m.RecipientId)
+                .IsRequired();
+
+            builder.HasOne(m => m.Trip)
+                .WithMany(t => t.Messages)
+                .HasForeignKey(m => m.TripId)
+                .IsRequired();
+        }
+    }
+}
