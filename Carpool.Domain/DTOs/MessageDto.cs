fe990505e3b961277917d3ce097faac2e4dbcb36
Axel Martin <martin.axel@sfr.fr>
2024-01-05 16:04:13 +0100

    Add Message dto

--- Carpool.Domain/DTOs/MessageDto.cs
+++ Carpool.Domain/DTOs/MessageDto.cs
@@ -1,18 +1,18 @@
-namespace Carpool.Domain.Entities
+namespace Carpool.Domain.DTOs
 {
-    public class Message
+    public class MessageDTO
     {
         public int Id { get; set; }
         public string Content { get; set; }
         public DateTime TimeStamp { get; set; }
-
+        
         public int SenderId { get; set; }
-        public User Sender { get; set; }
-
+        public string SenderName { get; set; }
+        
         public int RecipientId { get; set; }
-        public User Recipient { get; set; }
-
+        public string RecipientName { get; set; }
+        
         public int TripId { get; set; }
-        public Trip Trip { get; set; }
+        public string TripTitle { get; set; }
     }
 }
