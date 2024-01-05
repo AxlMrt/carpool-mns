780ea69f41bab919c35bccb15a3a03a22ba506aa
Axel Martin <martin.axel@sfr.fr>
2024-01-05 13:20:51 +0100

    Add data validation in Reservation service

--- Carpool.Application/Services/ReservationService.cs
+++ Carpool.Application/Services/ReservationService.cs
@@ -4,16 +4,21 @@ using Carpool.Application.Utils;
 using Carpool.Domain.DTO.Reservation;
 using Carpool.Domain.Entities;
 using Carpool.Domain.Interfaces;
+using Carpool.Infrastructure.Interfaces;
 
 namespace Carpool.Application.Services
 {
     public class ReservationService : IReservationService
     {
         private readonly IReservationRepository _reservationRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ITripRepository _tripRepository;
 
-        public ReservationService(IReservationRepository reservationRepository)
+        public ReservationService(IReservationRepository reservationRepository, IUserRepository userRepository, ITripRepository tripRepository)
         {
             _reservationRepository = reservationRepository;
+            _tripRepository = tripRepository;
+            _userRepository = userRepository;
         }
 
         public async Task<IEnumerable<ReservationDTO>> GetAllReservationsAsync()
@@ -65,6 +70,13 @@ namespace Carpool.Application.Services
         {
             if (createReservationDto == null)
                 throw new BadRequestException("Reservation object cannot be null.");
+            
+            
+            User user = await _userRepository.GetUserByIdAsync(createReservationDto.UserId) ?? throw new NotFoundException($"User with id {createReservationDto.UserId} not found.");
+            Trip trip = await _tripRepository.GetTripByIdAsync(createReservationDto.TripId) ?? throw new NotFoundException($"Trip with id {createReservationDto.TripId} not found.");
+
+            if (createReservationDto.ReservedSeats < 0 || trip.Car.NumberOfSeats < createReservationDto.ReservedSeats)
+                throw new BadRequestException("Invalid reserved seats.");
 
             Reservation reservation = ObjectUpdater.MapObject<Reservation>(createReservationDto);
             await _reservationRepository.CreateReservationAsync(reservation);
@@ -77,6 +89,9 @@ namespace Carpool.Application.Services
                 throw new BadRequestException("Invalid ID.");
 
             Reservation existingReservation = await _reservationRepository.GetReservationByIdAsync(id) ?? throw new NotFoundException($"Reservation with ID {id} not found.");
+
+            if (updateReservationDto.ReservedSeats <= 0 || existingReservation.ReservedSeats < updateReservationDto.ReservedSeats)
+                throw new BadRequestException("Invalid reserved seats.");
 
             existingReservation.ReservedSeats = updateReservationDto.ReservedSeats;
             existingReservation.Status = updateReservationDto.Status;
@@ -96,7 +111,5 @@ namespace Carpool.Application.Services
 
             return true;
         }
-
-        
     }
 }
