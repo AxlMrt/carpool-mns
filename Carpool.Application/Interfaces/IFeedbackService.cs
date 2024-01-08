f3c7a42930a5e90182fa183b67ccbe914fd9b17e
Axel Martin <martin.axel@sfr.fr>
2024-01-08 08:58:43 +0100

    Update ifeedbackservice

--- Carpool.Application/Interfaces/IFeedbackService.cs
+++ Carpool.Application/Interfaces/IFeedbackService.cs
@@ -1,15 +1,14 @@
 using Carpool.Domain.DTOs.Feedback;
-using Carpool.Domain.Entities;
 
 namespace Carpool.Application.Interfaces
 {
     public interface IFeedbackService
     {
-        Task<IEnumerable<Feedback>> GetAllFeedbacksAsync();
-        Task<Feedback> GetFeedbackByIdAsync(int id);
-        Task<IEnumerable<Feedback>> GetFeedbacksByUserIdAsync(int userId);
-        Task<Feedback> CreateFeedbackAsync(CreateFeedbackDTO feedback);
-        Task<Feedback> UpdateFeedbackAsync(int id, UpdateFeedbackDTO feedback);
+        Task<IEnumerable<FeedbackDTO>> GetAllFeedbacksAsync();
+        Task<FeedbackDTO> GetFeedbackByIdAsync(int id);
+        Task<IEnumerable<FeedbackDTO>> GetFeedbacksByUserIdAsync(int userId);
+        Task<FeedbackDTO> CreateFeedbackAsync(CreateFeedbackDTO feedback);
+        Task<FeedbackDTO> UpdateFeedbackAsync(int id, UpdateFeedbackDTO feedback);
         Task<bool> DeleteFeedbackAsync(int id);
     }
 }
